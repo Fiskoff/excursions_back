ebfbe2b018b9a16af6b3e2305a4ea6b5a4378255
Fiskoff <fiskoff2015@yandex.ru>
2024-10-14 19:02:26 +0700

    create user manager dependencies

---
+++ app/dependencies/user_manager.py
@@ -0,0 +1,8 @@
+from fastapi import Depends
+
+from core.authentication.user_manager import UserManager
+from app.dependencies.users import get_user_db
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
