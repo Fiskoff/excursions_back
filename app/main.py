ab850abdc2237040bd48ce2f82f1195ba70a5cf5
Fiskoff <fiskoff2015@yandex.ru>
2024-10-14 22:52:36 +0700

    create http bearer dependensy token

--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi import APIRouter
+from fastapi.security import HTTPBearer
 
 from app.router.auth_router import auth_router
+from app.router.register_router import register_router
+from app.router.user_router import user_router
 
 
 app = FastAPI()
 
-router = APIRouter(
-    prefix="/excursions",
-    tags=["Excursions"]
+http_bearer = HTTPBearer(auto_error=False)
+main_router = APIRouter(
+    prefix="/excursion",
+    dependencies=[Depends(http_bearer)],
 )
-router.include_router(auth_router)
+main_router.include_router(auth_router)
+main_router.include_router(register_router)
+main_router.include_router(user_router)
+
+
+app.include_router(main_router)
+
 
 if __name__ == "__main__":
     import uvicorn
